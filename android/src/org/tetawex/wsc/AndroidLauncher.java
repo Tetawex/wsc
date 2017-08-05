c2d7650e0de67cf8dfccba76f0c1c78e27ecc7e1
Tetawex <ILikeRedPower2>
2017-08-05 09:39:45 +0300

    Initial commit

---
+++ android/src/org/tetawex/wsc/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.tetawex.wsc;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.tetawex.wsc.WSCGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WSCGame(), config);
+	}
+}
---
+++ desktop/src/org/tetawex/wsc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.tetawex.wsc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.tetawex.wsc.WSCGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WSCGame(), config);
+	}
+}
